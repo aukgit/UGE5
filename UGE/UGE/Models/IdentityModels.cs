ca704e7026abb09fa4e0b3af16d8e91de2a29932
Alim Ul Karim <auk.junk@live.com>
2014-04-26 03:10:50 +0600

    Identity Database Change

---
+++ UGE/UGE/Models/IdentityModels.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace UGE.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder
+                .Properties()
+                .Where(n => n.Name.ToLower() == "id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+            .Where(x => x.Name == "Name")
+            .Configure(c => c.HasMaxLength(30));
+
+            modelBuilder.Properties<DateTime>()
+            .Configure(c => c.HasColumnType("date"));
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(n => n.Email)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(n => n.UserName)
+                .IsUnicode(false)
+                .HasMaxLength(30);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(n => n.PasswordHash)
+                .IsUnicode(false)
+                .HasMaxLength(70);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(n => n.SecurityStamp)
+                .IsUnicode(false)
+                .HasMaxLength(38);
+
+            modelBuilder.Entity<ApplicationUser>()
+              .Property(n => n.Id)
+              .IsUnicode(false)
+              .HasMaxLength(38);
+          
+
+        }
+
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
